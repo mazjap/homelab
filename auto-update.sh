63254e9f9a683d8094f293bdf893ddaac84ff697
Jordan C. <jordan.c4922@gmail.com>
2026-02-27 16:29:33 -0700

    Add linkding bookmark manager and update relavent files

--- auto-update.sh
+++ auto-update.sh
@@ -88,7 +88,7 @@ cd /home/jman/pistack
 log "Pulling latest images..."
 
 # Pull everything, except 4get (bc it's not in the registry since it's manually compiled)
-docker compose pull pihole unbound wg-easy homepage uptime-kuma portainer nginx-proxy-manager glances >> "$LOG_FILE" 2>&1
+docker compose pull pihole unbound wg-easy homepage uptime-kuma portainer nginx-proxy-manager glances linkding >> "$LOG_FILE" 2>&1
 
 if [ $? -ne 0 ]; then
     log "ERROR: Failed to pull images, aborting container update"
